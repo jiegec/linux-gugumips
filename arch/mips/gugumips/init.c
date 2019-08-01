20a8136ce6a43ca19f27d67aea50be764276a327
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-01 14:55:16 +0800

    Fix irq cascading and cleanup unneeded code

--- arch/mips/gugumips/init.c
+++ arch/mips/gugumips/init.c
@@ -1,4 +1,5 @@
 #include <linux/of_fdt.h>
+#include <linux/of_platform.h>
 #include <linux/clk-provider.h>
 #include <linux/irqchip.h>
 #include <linux/clk.h>
@@ -7,6 +8,7 @@
 #include <asm/irq_cpu.h>
 #include <asm/time.h>
 #include <asm/mips-cps.h>
+#include <asm/irq_cpu.h>
 
 void __init plat_mem_setup(void)
 {
@@ -39,22 +41,15 @@ void __init plat_time_init(void) {
 
     mips_hpt_frequency = clk_get_rate(clk);
     clk_put(clk);
-    timer_probe();
 }
 
 void __init arch_init_irq(void) {
-    unsigned int sr;
-    struct device_node *intc_node;
+	unsigned int sr;
+	sr = set_c0_status(ST0_BEV);
+	write_c0_ebase((u32)ebase);
+	write_c0_status(sr);
 
-    sr = set_c0_status(ST0_BEV);
-    write_c0_ebase((u32)ebase);
-    write_c0_status(sr);
-
-    intc_node = of_find_compatible_node(NULL, NULL,
-                        "mti,cpu-interrupt-controller");
-    of_node_put(intc_node);
-
-    irqchip_init();
+	irqchip_init();
 }
 
 const char *get_system_type(void)
@@ -69,6 +64,14 @@ const char *get_system_type(void)
     return "Unknown";
 }
 
-unsigned int get_c0_compare_int(void) {
-    return CP0_LEGACY_COMPARE_IRQ;
+static int __init plat_of_setup(void)
+{
+	if (!of_have_populated_dt())
+		panic("Device tree not present");
+
+	if (of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL))
+		panic("Failed to populate DT");
+
+	return 0;
 }
+arch_initcall(plat_of_setup);
